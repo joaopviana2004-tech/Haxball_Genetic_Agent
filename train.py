fb758d99f675fb8a2f107b2f69ccb09c79015fd0
Kaio.w <kaiowellinghton@gmail.com>
2025-12-11 19:27:59 -0300

    Changes to be committed: 	modified:   train.py

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ for y in range(config.ROWS):
         begin = (coord_x_inicial,  coord_y_inicial)
         final = (coord_x_final,  coord_y_final)
         
-        quadras.append(Quadra(screen, begin, final , ['player', 'bot']))
+        quadras.append(Quadra(screen, begin, final , []))
 
 clock = pygame.time.Clock()
 running = True
@@ -40,6 +40,7 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
+    screen.fill((0, 0, 0))
     [quadra.update() for quadra in quadras]
     pygame.display.flip()
 
